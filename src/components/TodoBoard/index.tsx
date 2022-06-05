d87c05ed0a0834a0de0e9f86e192764fd4c8d8eb
Min Jiwon <peace.cheejecake@gmail.com>
2022-06-05 23:30:28 +0900

    refactor: Modularize TodoBoard

---
+++ src/components/TodoBoard/index.tsx
@@ -0,0 +1,31 @@
+import { useState } from 'react';
+import { useRecoilState } from 'recoil';
+import { itemList } from 'store/atoms/itemList';
+import TodoItem from 'components/TodoBoard/TodoItem';
+import { AddIcon } from 'assets/svgs';
+import { createNewItem } from './TodoItem/_utils';
+import styles from './todoBoard.module.scss';
+
+export default function TodoBoard() {
+  const [items, setItems] = useRecoilState(itemList);
+  const [selectedId, setSelectedId] = useState(-1);
+
+  const handleClickAdd = () => {
+    const newItem = createNewItem(items.length);
+    setItems((prev) => [...prev, newItem]);
+  };
+
+  return (
+    <>
+      <p className={styles.title}>
+        Quick Todo
+        <AddIcon className={styles.addIcon} onClick={handleClickAdd} />
+      </p>
+      <div className={styles.todos}>
+        {items.map((item) => (
+          <TodoItem key={item.id} item={item} selectedId={selectedId} setSelectedId={setSelectedId} />
+        ))}
+      </div>
+    </>
+  );
+}
--- src/routes/Todo/index.tsx
+++ src/routes/Todo/index.tsx
@@ -1,34 +1,13 @@
-import { useState } from 'react';
-import { useRecoilState } from 'recoil';
 import Layout from 'components/layouts';
+import TodoBoard from 'components/TodoBoard';
 import Card from 'components/_common/Card';
-import { itemList } from 'store/atoms/itemList';
-import TodoItem from 'routes/Todo/TodoItem';
-import { AddIcon } from 'assets/svgs';
 import styles from './todo.module.scss';
-import { createNewItem } from './TodoItem/_utils';
 
 export default function Todo() {
-  const [items, setItems] = useRecoilState(itemList);
-  const [selectedId, setSelectedId] = useState(-1);
-
-  const handleClickAdd = () => {
-    const newItem = createNewItem(items.length);
-    setItems((prev) => [...prev, newItem]);
-  };
-
   return (
     <Layout>
       <Card className={styles.quickTodo}>
-        <p className={styles.title}>
-          Quick Todo
-          <AddIcon className={styles.addIcon} onClick={handleClickAdd} />
-        </p>
-        <div className={styles.todos}>
-          {items.map((item) => (
-            <TodoItem key={item.id} item={item} selectedId={selectedId} setSelectedId={setSelectedId} />
-          ))}
-        </div>
+        <TodoBoard />
       </Card>
     </Layout>
   );
