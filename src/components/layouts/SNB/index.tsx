39c97747e0646128c0ed1e6c5b1e64374c092fb4
Min Jiwon <peace.cheejecake@gmail.com>
2022-06-12 21:40:22 +0900

    fix: Sync theme change timing

--- src/components/layouts/SNB/index.tsx
+++ src/components/layouts/SNB/index.tsx
@@ -1,23 +1,21 @@
 import { useReducer } from 'react';
-import { useRecoilValue } from 'recoil';
 import cx from 'classnames';
 
 import { ArrowBackIcon, ArrowForwardIcon, ChecklistIcon, DashboardIcon, NoteIcon } from 'assets/svgs';
-import { darkModeState } from 'store/states/themeState';
+import { useDarkMode } from 'hooks';
 import Button from './_components/Button';
 import styles from './snb.module.scss';
 
 export default function SNB() {
   const [isOpen, toggleIsOpen] = useReducer((prev) => !prev, true);
-  const darkMode = useRecoilValue(darkModeState);
+  const [, themeClassName] = useDarkMode();
 
   const Icon = isOpen ? ArrowBackIcon : ArrowForwardIcon;
 
   const openClassName = isOpen ? styles.open : styles.closed;
-  const themeClassName = darkMode ? styles.darkMode : styles.lightMode;
 
   return (
-    <nav className={cx(styles.wrapper, openClassName, themeClassName)}>
+    <nav className={cx(styles.wrapper, openClassName, styles[themeClassName])}>
       <div className={styles.snbMenu}>
         <Button Icon={DashboardIcon} title="Dashboard" path="dashboard" />
         <Button Icon={ChecklistIcon} title="Todos" path="todos" />
